c5e1e6d3fa5f7788191c824a1e366334335d2de3
LilianKanyua <liliankanyua@gmail.com>
2023-03-17 10:14:22 +0300

    commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,16 @@
 fun main(){
    numbers(1..100)
+
+    var ray=array(arrayOf("brother","kid","book","flower"))
+    println(ray)
+
     serve(4)
     serve(12)
     serve(25)
-    //myNum(1..100)
-    var ray=array(arrayOf("brother,kid,book,flower"))
-    println(ray)
+
+    num1(1..100)
 }
+
 fun numbers(numbers:IntRange) {
     for (num in numbers) {
        if (num%2!==0){
@@ -21,11 +25,9 @@ fun numbers(numbers:IntRange) {
         var numLong=0
         for (longest in myArr) {
             if (longest.length > 5){
-           ++numLong
+           numLong++
             }
-            //else{
-                //numLong=0
-            //}
+
         }
         return numLong
     }
@@ -41,24 +43,14 @@ fun serve(age:Int){
  }
 
 }
- //fun myNum(nums:IntRange) {
-   //  for (num in nums) {
-     //    if (num % 3 == 0) {
-       //      println("Fizz")
-         //} else if (num % 5 == 0) {
-           //  println("Buzz")
-         //} else if (num % 3 == 0 && num % 5 == 0) {
-           //  println("FizzBuzz")
-         //}
-        // else{
-          //   println("not applicable")
-         //}
-     //}
- //}
+
+
 fun num1(nums:IntRange){
     for (num in nums){
-        when(num){
-
+        when {
+            num %3==0 && num %5==0 ->  println("FizzBuzz")
+            num %3==0 -> println("Fizz")
+            num %5==0 -> println("Buzz")
         }
     }
 }
